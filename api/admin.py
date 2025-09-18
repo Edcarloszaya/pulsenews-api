5980dde369fa1fb6f35a5d97522bfdeef3188923
Edcarloszaya <edcarlos.molequedoido@gmail.com>
2025-09-18 12:50:42 -0300

    feat: registro dos models no painel admin

---
+++ api/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import ApiKey, News
+
+# Register your models here.
+admin.site.register(ApiKey)
+# Register your models here.
+admin.site.register(News)
