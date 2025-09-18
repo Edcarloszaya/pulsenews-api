6ba771f875925822d44631665930e1541605751f
Edcarloszaya <edcarlos.molequedoido@gmail.com>
2025-09-18 13:16:17 -0300

    fix: corrigido erro de schema com import no AppConfig

---
+++ api/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class ApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "api"
+
+    def ready(self):
+        # Importa o schema para registrar no momento de inicialização
+        import api.schema
